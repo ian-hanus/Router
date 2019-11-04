f7897c9a9b98f7ce1a151c87d6d3e78e5240b05c
ian-hanus <ianhanus@gmail.com>
2019-11-04 13:47:09 -0500

    Making sure git updated to submission

--- sr_arpcache.c
+++ sr_arpcache.c
@@ -77,7 +77,53 @@ void sr_arpcache_sweepreqs(struct sr_instance *sr) {
 			printf("NEED TO RESENT OR DELETE\n");
 			if(req->times_sent>=5){
 				printf("DELETING\n");
+				sr_ip_hdr_t* header = (sr_ip_hdr_t*)(req->packets + sizeof(sr_ethernet_hdr_t));
+				int oldSum = header->ip_sum;
+				struct sr_if* current = sr->if_list;
+				struct sr_rt* rt_walker = sr->routing_table;
+				while(rt_walker!=NULL){
+					printf("CHECKING RT\n");
+					printf("%s\n",inet_ntoa(rt_walker->dest));
+					if((header->ip_dst)==rt_walker->dest.s_addr){
+						printf("FOUND MATCH\n");
+						while(current!=NULL){
+							printf("%s vs. %s\n",current->name,rt_walker->interface);
+							if(strcmp(current->name,rt_walker->interface)==0){
+								printf("FOUND MATCHING INTERFACE\n");
+								break;
+							}
+							current=current->next;
+						}
+						break;
+					}
+					rt_walker=rt_walker->next;
+				}
+				void* errPacket=malloc(sizeof(sr_ethernet_hdr_t)+sizeof(sr_ip_hdr_t)+sizeof(sr_icmp_t3_hdr_t));
+				sr_ethernet_hdr_t* ethhdr= (sr_ethernet_hdr_t*)req->packets;
+				sr_ethernet_hdr_t* errEthHdr=(sr_ethernet_hdr_t*)errPacket;
+				 memcpy(errEthHdr->ether_dhost,ethhdr->ether_shost,ETHER_ADDR_LEN);
+				 memcpy(errEthHdr->ether_shost,ethhdr->ether_dhost,ETHER_ADDR_LEN);
+				 errEthHdr->ether_type=htons(ethertype_ip);
+				 sr_ip_hdr_t* errIPhdr=(sr_ip_hdr_t*)(errPacket+sizeof(sr_ethernet_hdr_t));
+				 memcpy(errIPhdr,header,sizeof(sr_ip_hdr_t));
+				 errIPhdr->ip_p=ip_protocol_icmp;
+				 errIPhdr->ip_len=htons(sizeof(sr_ip_hdr_t)+sizeof(sr_icmp_t3_hdr_t));
+				 errIPhdr->ip_dst=header->ip_src;
+				 errIPhdr->ip_src=header->ip_dst;
+				 sr_icmp_t3_hdr_t* icmpHdr=(sr_icmp_t3_hdr_t*)(errPacket+sizeof(sr_ethernet_hdr_t)+sizeof(sr_ip_hdr_t));
+				 icmpHdr->icmp_code=1;
+				 icmpHdr->icmp_type=3;
+				 icmpHdr->icmp_sum=0;
+				 memcpy(icmpHdr->data,header,ICMP_DATA_SIZE);
+				 icmpHdr->icmp_sum=cksum(icmpHdr,sizeof(sr_icmp_t3_hdr_t));
+				 printf("MADE HEADER\n");
+				 errIPhdr->ip_sum=0;
+				 errIPhdr->ip_sum=cksum(errIPhdr,sizeof(sr_ip_hdr_t)+sizeof(sr_icmp_t3_hdr_t));
+				 print_hdrs(errPacket,sizeof(sr_ethernet_hdr_t)+sizeof(sr_ip_hdr_t)+sizeof(sr_icmp_t3_hdr_t));
+
+				 sr_send_packet(sr , errPacket , sizeof(sr_ethernet_hdr_t)+sizeof(sr_ip_hdr_t)+sizeof(sr_icmp_t3_hdr_t), rt_walker->interface);
 				sr_arpreq_destroy(&(sr->cache),req);
+
 				printf("DELETED\n");
 			}else{
 				printf("RESENDING\n");
